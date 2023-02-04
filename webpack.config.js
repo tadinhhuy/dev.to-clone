e448caa8e4335bf962abb32bc9faac58d69d3f01
HuyTa <tadinhhuy907@gmail.com>
2023-02-05 01:26:18 +0700

    feat: add plugins to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,142 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/order */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
+const getProductionConfig = () => {
+  return {
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CompressionPlugin({
+        test: /\.(css|js)$/,
+        algorithm: 'brotliCompress'
+      }),
+      new CleanWebpackPlugin(),
+      new CssMinimizerPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'static/css/[name].[contenthash:6].css'
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+    devtool: false,
+  }
+};
+
+/** @type {(env: any, arg: {mode: string}) => import('webpack').Configuration} **/
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  const config = {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'static/js/bundle.[contenthash:6].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.(s[ac]ss|css)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader" },
+            {
+              loader: "postcss-loader", options: {
+                postcssOptions: {
+                  path: __dirname,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: isProduction ? 'static/media/[name].[contenthash:6].[ext]' : '[path][name].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: isProduction ? 'static/fonts/[name].[ext]' : '[path][name].[ext]'
+              }
+            }
+          ]
+        }
+      ],
+    },
+    resolve: {
+      alias: {
+        ['@']: path.resolve(__dirname, 'src')
+      },
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      }),
+      new Dotenv(),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'public',
+            to: '.',
+            filter: (name) => !name.endsWith('index.html')
+          }
+        ]
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+        filename: 'index.html'
+      }),
+      new ESLintPlugin({
+        extensions: ['.tsx', '.ts', '.js', '.jsx']
+      }),
+    ],
+    devServer: {
+      hot: true,
+      historyApiFallback: true,
+      port: 8080,
+      static: {
+        directory: path.resolve(__dirname, 'public', 'index.html'),
+        serveIndex: true,
+        watch: true
+      }
+    },
+    devtool: 'source-map',
+  }
+
+  if (isProduction) {
+    const productionConfig = getProductionConfig();
+    const plugins = [...config.plugins, ...productionConfig.plugins];
+    const devtool = productionConfig.devtool;
+
+    return {
+      ...config,
+      plugins,
+      devtool
+    }
+  }
+
+  return config;
 };
