706dacf7bf89d6ad5891f4aec6c386ff477d8859
Nguyet Phung <nguyetphung@Nguyets-MacBook-Pro.local>
2023-02-09 00:37:19 +0700

    feat: implement routes config

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
-import Home from './Home';
+import Home from './home';
 
 export { Home };
+
+export * from './user';
---
+++ src/pages/user/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const User: React.FC = () => {
+  return <div className="text-3xl font-bold underline">Protected Page</div>;
+};
+
+export default User;
