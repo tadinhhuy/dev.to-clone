d0a8535098f2eccc0cfa0fd8aba0a9f1c4570afb
HuyTa <tadinhhuy907@gmail.com>
2023-06-03 02:20:34 +0700

    style: add position fixed to header page

--- src/components/Header/search-input.comp.tsx
+++ src/components/Header/search-input.comp.tsx
@@ -1,40 +1,51 @@
+import clsx from 'clsx';
 import { ChangeEvent, KeyboardEvent, memo, useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import Button from '@/components/button';
 
 const ENTER_KEY = 'Enter';
 
 const SearchInput = ({ onSearch }: { onSearch: (value: string) => void }) => {
   const [value, setValue] = useState('');
 
-  const handleSearch = () => {
-    onSearch(value);
-  };
-
   const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   const handleKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === ENTER_KEY) {
-      handleSearch();
+    if (e.key === ENTER_KEY && value.trim()?.length > 0) {
+      onSearch(value);
+    }
+  };
+
+  const handleClick = () => {
+    if (value.trim()?.length > 0) {
+      onSearch(value);
     }
   };
 
   return (
-    <div className="ml-4 flex w-full items-center rounded-md border-[1.5px] border-gray-300">
+    <div
+      className={clsx(
+        'relative ml-4 flex w-full items-center rounded-md border-[1.5px] border-gray-300 px-2 py-[6.5px]',
+        'focus-within:border-[1.5px] focus-within:border-blue-700'
+      )}
+    >
       <input
-        className="w-full p-1.5 text-sm focus:outline-none"
+        className="text-md w-full placeholder:text-gray-600 focus:outline-none"
         type="text"
         placeholder="Search..."
         value={value}
         onChange={handleChangeSearch}
         onKeyDown={handleKeydown}
       />
-      <button onClick={handleSearch} type="submit" className="mx-2">
+      <Button onClick={handleClick} variant="text" className="absolute right-0 h-full px-2 py-0">
         <FiSearch className="h-6 w-6" />
-      </button>
+      </Button>
     </div>
   );
 };
 
 export default memo(SearchInput);
+
+SearchInput.displayName = 'SearchInput';
