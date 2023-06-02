9b3193d9a18ab23fd7adae1cc83b00191a5864ca
HuyTa <tadinhhuy907@gmail.com>
2023-06-03 02:21:13 +0700

    chore: update global-layout

--- src/components/layouts/global-layout/global-layout.comp.tsx
+++ src/components/layouts/global-layout/global-layout.comp.tsx
@@ -4,8 +4,10 @@ const GlobalLayout = ({ children }: { children: JSX.Element }) => {
   return (
     <div>
       <Header />
-      <div className="mx-24 min-h-[calc(100vh-5rem)]">{children}</div>
-      <div className='h-60 bg-neutral-200 p-10'>Footer</div>
+      <div className="bg-neutral-100">
+        <main className="mx-[106px] mt-14 min-h-[calc(100dvh-3.5rem)] p-4">{children}</main>
+      </div>
+      <footer className="h-60 bg-neutral-200 p-10">Footer</footer>
     </div>
   );
 };
